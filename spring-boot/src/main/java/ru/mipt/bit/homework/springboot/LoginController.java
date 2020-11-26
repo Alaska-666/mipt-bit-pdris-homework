9ffb04ebf30d75fc83edefa00409bb3bc18f6d56
isaeva anna <pangolin-666@mail.ru>
2020-11-26 13:06:18 +0300

    fixed remarks

--- spring-boot/src/main/java/ru/mipt/bit/homework/springboot/LoginController.java
+++ spring-boot/src/main/java/ru/mipt/bit/homework/springboot/LoginController.java
@@ -3,6 +3,7 @@ package ru.mipt.bit.homework.springboot;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -10,15 +11,10 @@ import javax.validation.Valid;
 public class LoginController {
     UserStorage userStorage = new UserStorage();
     LogStorage logStorage = new LogStorage();
-    private String loginUsername = "";
-    private String registrationUsername = "";
-    private String message = "";
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registration(ModelMap model) {
-        model.addAttribute("username", registrationUsername);
-        model.addAttribute("user", new User());
-        registrationUsername = "";
+        addAttributes(model, new User(), "", "");
         return "registration";
     }
 
@@ -29,9 +25,9 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String submitRegistration(@Valid @ModelAttribute("user")User user, ModelMap model) {
+    public String submitRegistration(@Valid @ModelAttribute("user")User user, ModelMap model, RedirectAttributes redirectAttributes) {
+        String username = user.getUsername();
         if (user.getUsername().equalsIgnoreCase("admin") || userStorage.isUserExist(user)) {
-            registrationUsername = user.getUsername();
             String action = "";
             if (user.getUsername().equalsIgnoreCase("admin")) {
                 action = "registered with the name 'admin'";
@@ -39,31 +35,43 @@ public class LoginController {
                 action = "registered with the occupied name";
             }
             logStorage.add(user, action, "FAIL");
-            return registration(model);
+            addAttributes(model, new User(), username, "");
+            return "registration";
         }
         else {
             userStorage.add(user);
-            message = "registration";
-            loginUsername = user.getUsername();
-            registrationUsername = "";
+            addAttributes(redirectAttributes, new User(), username, "registration");
             logStorage.add(user, "registration", "OK");
             return "redirect:/";
         }
     }
 
+    private void addAttributes(ModelMap model, User user, String username, String message) {
+        model.addAttribute("user", user);
+        model.addAttribute("username", username);
+        model.addAttribute("message", message);
+    }
+
+    private void addAttributes(RedirectAttributes redirectAttributes, User user, String username, String message) {
+        redirectAttributes.addFlashAttribute("user", user);
+        redirectAttributes.addFlashAttribute("username", username);
+        redirectAttributes.addFlashAttribute("message", message);
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String login(ModelMap model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("username", loginUsername);
-        model.addAttribute("message", message);
-        loginUsername = "";
-        message = "";
+        if (model.isEmpty()) {
+            addAttributes(model, new User(), "", "");
+        } else {
+            addAttributes(model, new User(), (String) model.getAttribute("username"), (String) model.getAttribute("message"));
+        }
         return "login";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public String submitLogin(@Valid @ModelAttribute("user")User user, ModelMap model) {
-        loginUsername = user.getUsername();
+        String username = user.getUsername();
+        String message = "";
         if (!userStorage.isUserExist(user)) {
             message = "notExist";
             logStorage.add(user, "attempt to authorize an unregistered user", "FAIL");
@@ -74,11 +82,10 @@ public class LoginController {
         }
         else {
             logStorage.add(user, "authorization", "OK");
-            model.addAttribute("username", loginUsername);
-            message = "";
-            loginUsername = "";
+            model.addAttribute("username", username);
             return "pageAfterLogin";
         }
-        return login(model);
+        addAttributes(model, user, username, message);
+        return "login";
     }
 }
